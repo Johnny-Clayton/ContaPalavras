0b58255483ab232b4917b0db274f1d29c8b719fc
Johnny <delga300@hotmail.com>
2023-08-14 19:54:00 -0300

    Ajuste no código e adiciona README detalhado em português e inglês

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ContaPalavras
+# Contador de Palavras (Word Counter)
+
+Uma aplicação Java simples que lê um arquivo e conta a frequência de cada palavra no arquivo, apresentando os resultados em uma interface gráfica.
+
+## 🛠️ Tecnologias Usadas
+
+- **Java**
+- **Swing** para a interface gráfica
+
+## 📖 Explicação do Código
+
+Esta aplicação foi desenvolvida para fornecer uma maneira rápida e eficiente de contar palavras em um arquivo de texto. A razão pela qual escolhemos usar Java é devido à sua portabilidade e rica biblioteca padrão, que facilita a leitura de arquivos e a criação de interfaces gráficas. O Swing, embora seja um pouco mais antigo, oferece uma maneira rápida e direta de criar interfaces gráficas para aplicações desktop.
+
+## 🔄 Como Funciona
+
+1. A interface permite que o usuário selecione um arquivo através de um botão.
+2. Depois de selecionar o arquivo, o programa lê o conteúdo e divide em palavras.
+3. Ele conta a ocorrência de cada palavra e as apresenta em ordem decrescente de frequência em um `JTable`.
+
+## 🚀 Como Usar
+
+1. Compile e execute o programa.
+2. Clique no botão "Carregar Arquivo" e selecione o arquivo que deseja analisar.
+3. Os resultados serão exibidos na tabela.
+
+---
+
+# Word Counter
+
+A simple Java application that reads a file and counts the frequency of each word in the file, presenting the results in a graphical interface.
+
+## 🛠️ Technologies Used
+
+- **Java**
+- **Swing** for the graphical interface
+
+## 📖 Code Explanation
+
+This application was designed to provide a quick and efficient way to count words in a text file. The reason we chose to use Java is due to its portability and rich standard library, which makes it easy to read files and create graphical interfaces. Swing, although a bit older, offers a quick and straightforward way to create graphical interfaces for desktop applications.
+
+## 🔄 How It Works
+
+1. The interface allows the user to select a file through a button.
+2. After selecting the file, the program reads its content and splits it into words.
+3. It counts the occurrence of each word and presents them in descending order of frequency in a `JTable`.
+
+## 🚀 How to Use
+
+1. Compile and run the program.
+2. Click on the "Load File" button and select the file you wish to analyze.
+3. The results will be displayed on the table.
--- src/arquivo/ContaPalavras.java
+++ src/arquivo/ContaPalavras.java
@@ -1,5 +1,8 @@
 package arquivo;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,44 +11,94 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
 
 public class ContaPalavras {
-  public static void main(String[] args) {
-	  
-	Scanner sc = new Scanner(System.in);
-	  
-	System.out.println("Digite um caminho de pasta: ");
-	
-    String fileName = sc.nextLine(); 
-    
-    HashMap<String, Integer> contadorDePalavras = new HashMap<>();
-    
-    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-    	  String linha = reader.readLine();
-    	  while (linha != null) {
-    	    String[] palavras = linha.split(" ");
-    	    for (String palavra : palavras) {
-    	      if (contadorDePalavras.containsKey(palavra)) {
-    	        int contador = contadorDePalavras.get(palavra);
-    	        contadorDePalavras.put(palavra, contador + 1);
-    	      } else {
-    	        contadorDePalavras.put(palavra, 1);
-    	      }
-    	    }
-    	    linha = reader.readLine();
-    	  }
-	} catch (IOException e) {
-	  e.printStackTrace();
-	}
-    
-    List<Map.Entry<String, Integer>> entradas = new ArrayList<>(contadorDePalavras.entrySet());
-    entradas.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
-    for (Map.Entry<String, Integer> entrada : entradas) {
-    String palavra = entrada.getKey();
-    int contador = entrada.getValue();
-    System.out.println(palavra + "' aparece " + contador + " vezes no texto.");
+    private JFrame frame;
+    private JTable table;
+    private JButton loadButton;
+    private JFileChooser fileChooser;
+    private DefaultTableModel tableModel;
+
+    public ContaPalavras() {
+        frame = new JFrame("Conta Palavras");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLayout(new BorderLayout());
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Palavra");
+        tableModel.addColumn("Quantidade");
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        loadButton = new JButton("Carregar Arquivo");
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openFile();
+            }
+        });
+
+        fileChooser = new JFileChooser();
+
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.add(loadButton, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
     }
-  }
+
+    private void openFile() {
+        int returnValue = fileChooser.showOpenDialog(frame);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            String fileName = fileChooser.getSelectedFile().getAbsolutePath();
+            countWords(fileName);
+        }
+    }
+
+    private void countWords(String fileName) {
+        HashMap<String, Integer> contadorDePalavras = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String linha = reader.readLine();
+            while (linha != null) {
+                String[] palavras = linha.split(" ");
+                for (String palavra : palavras) {
+                    if (contadorDePalavras.containsKey(palavra)) {
+                        int contador = contadorDePalavras.get(palavra);
+                        contadorDePalavras.put(palavra, contador + 1);
+                    } else {
+                        contadorDePalavras.put(palavra, 1);
+                    }
+                }
+                linha = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<Map.Entry<String, Integer>> entradas = new ArrayList<>(contadorDePalavras.entrySet());
+        entradas.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        tableModel.setRowCount(0); 
+        for (Map.Entry<String, Integer> entrada : entradas) {
+            tableModel.addRow(new Object[]{entrada.getKey(), entrada.getValue()});
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ContaPalavras();
+            }
+        });
+    }
 }
